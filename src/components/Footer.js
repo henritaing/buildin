04857f0bfcadcbe8a37168aed6689310f2506ce1
henritaing <95174330+henritaing@users.noreply.github.com>
2025-01-19 14:59:29 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className='footer-container'>
+      <div class='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>A propos</h2>
+            <Link to='/sign-up'>Nous rejoindre</Link>
+            <Link to='/'>Témoignages</Link>
+            <Link to='/'>Carrières</Link>
+            <Link to='/'>Conditions de service</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Contact</h2>
+            <Link to='/'>Nous contacter</Link>
+            <Link to='/'>Nos sponsors</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>Nos réseaux</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>LinkedIn</Link>
+            <Link to='/'>Youtube</Link>
+          </div>
+        </div>
+    </div>
+      <section class='social-media'>
+        <div class='social-media-wrap'>
+          <medium class='website-rights'>BUILDIN © 2025</medium>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default Footer className="footer-container";
+export default Footer;
---
+++ src/components/Presentation.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Presentation.css';
+
+function Presentation() {
+  return (
+    <div>Presentation</div>
+  )
+}
+
+export default Presentation;
